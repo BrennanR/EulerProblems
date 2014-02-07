4c8582a0134c8d2dc98187a9cc5b47e2e6997b67
Brennan Rauert <brennanrauert@gmail.com>
2014-02-06 22:09:16 -0600

    Fixed some problems I unintentionally introduced when trying to speed up the solution. Added a run time print statement. Run time is about 90 seconds.

--- euler9.py
+++ euler9.py
@@ -1,11 +1,10 @@
 __author__ = 'Brennan'
 
 import itertools
-import math
 import time
 
 
-def is__pythagorean_triplet(a, b, c):
+def is_pythagorean_triplet(a, b, c):
     return a * a + b * b == c * c
 
 
@@ -14,13 +13,12 @@ def find_triplet_that_sums_to(triple_sum):
     all_ordered_triples = itertools.ifilter(lambda x: x[0] < x[1] < x[2], all_triples)
 
     for triple in all_ordered_triples:
-        if is__pythagorean_triplet(triple[0], triple[1], triple[2]):
+        if is_pythagorean_triplet(triple[0], triple[1], triple[2]):
             if triple[0] + triple[1] + triple[2] == triple_sum:
-                return math.sqrt(triple[0]) * math.sqrt(triple[1]) * math.sqrt(triple[2])
+                return triple[0] * triple[1] * triple[2]
 
 
-time1 = time.time()
+start_time = time.time()
 print find_triplet_that_sums_to(1000)
-time2 = time.time()
-print("Time 2: " + str(time2))
-print("Run time: " + str(time2 - time1))
+end_time = time.time()
+print("Run time: {0} seconds.".format(str(end_time - start_time)))
